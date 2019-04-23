8f03efa547add12e86966a3418ef0a586f5cd34c
Florent330 <Florent_Duval_student2019@wilder.school>
2019-04-23 12:56:40 +0200

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -12,27 +12,5 @@ if (isset($_POST['loginName']) && !empty($_POST['loginName'])) {
 
 
 
-//else {
-
- // header('location:login.php');
-
-//}
-/* if (empty($_SESSION['loginName']) || !isset($_SESSION['loginName'])){
-
-    header('location:login.php');
-}
-
-
-
-
-if (isset($_POST['loginName']) && !empty($_POST['loginName'])) {
-
-    session_start();
-
-    $_SESSION['loginName'] = $_POST['loginName'];
-
-    header('location:index.php');
-
-
 
 
